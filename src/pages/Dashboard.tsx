287a69095cdd5129ef6ec50a25c686309ee65604
muntashir-wahid <muntashir.wahid@yahoo.com>
2026-06-10 13:24:25 +0600

    used fetched drops data from the server instead of mock data

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
+
+import api from "../lib/http/axios";
+import type { Drop } from "../types/drop";
 import DropCard from "../components/DropCard";
-import { mockDrops } from "../mocks/drops";
 
 const Dashboard = () => {
-  const data = mockDrops;
+  const [data, setData] = useState<Drop[] | []>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    api
+      .get("/drops")
+      .then((response) => {
+        const drops = response.data as Drop[];
+        setData(drops);
+      })
+      .catch((error) => {
+        console.error("Error fetching drops:", error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <div className="p-6">Loading...</div>;
+  }
 
   return (
     <div className="p-6 grid gap-4 md:grid-cols-2">
